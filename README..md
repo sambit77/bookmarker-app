2cba7a47c420e1827b4a1ec4b52f55efb2966046
Sambit Pradhan <sambitgulu@gmail.com>
2024-05-23 00:47:38 +0530

    docker-compose env setup

---
+++ README..md
@@ -0,0 +1,19 @@
+
+
+## How to run?
+
+```shell
+$ git clone https://github.com/sambit77/bookmarker-app
+$ cd bookmarker-app
+$ ./run.sh start
+$ ./run.sh stop
+
+$ ./run.sh start_infra
+$ ./run.sh stop-infra
+```
+
+* To start only dependent services
+```shell
+$ ./run.sh start_infra
+$ ./run.sh stop-infra
+```
---
+++ run.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+declare dc_infra=docker-compose.yml
+declare dc_app=docker-compose-app.yml
+
+function build_api() {
+  cd bookmarker-api
+  ./mvnw clean package -DskipTests
+  cd ..    
+}
+
+function start_infra() {
+    echo "starting infra docker containers ...."
+    docker-compose -f ${dc_infra} up -d
+    docker-compose -f ${dc_infra} logs -f
+}
+
+function stop_infra() {
+     echo "stopping infra docker containers ...."
+        docker-compose -f ${dc_infra} stop
+        docker-compose -f ${dc_infra} rm -f
+}
+
+function start() {
+
+    build_api
+    echo "starting all docker containers"
+    docker-compose -f ${dc_infra} -f ${dc_app} up --build -d
+    docker-compose -f ${dc_infra} -f ${dc_app} logs -f
+}
+
+function stop() {
+    echo "stop all docker containers"
+    docker-compose -f ${dc_infra} -f ${dc_app} stop
+    docker-compose -f ${dc_infra} -f ${dc_app} rm -f
+}
+
+function restart() {
+    stop
+    sleep 3
+    start
+}
+
+
+action="start"
+
+if [[ "$#" != "0" ]]
+then
+    action=$@
+fi
+
+eval ${action}
